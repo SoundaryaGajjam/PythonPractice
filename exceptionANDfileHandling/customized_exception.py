42c46da85b8bc05045e36a0a5dd25e90f801e309
SoundaryaGajjam <soundaryagajjam@gmail.com>
2019-08-22 17:18:22 +0530

    User defined exception

---
+++ exceptionANDfileHandling/customized_exception.py
@@ -0,0 +1,14 @@
+class TooYoungeException(Exception):
+    def __init__(self,arg):
+        self.msg=arg
+
+class TooOldException(Exception):
+    def __init__(self,arg):
+        self.msg=arg
+age=int(input("Enter age :"))
+if age>60:
+    raise TooYoungeException("Plz wait some more time definately you will get best match soon")
+elif age<18:
+    raise TooOldException("Ur age already crossed marriage age no chance of getting marriage")
+else:
+    print("Thanks for registration")
