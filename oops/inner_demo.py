2f56a4f2cd3842a76d71c64afbbf15fe7f89b08a
SoundaryaGajjam <soundaryagajjam@gmail.com>
2019-08-29 12:53:49 +0530

    Inner cls

--- oops/inner_demo.py
+++ oops/inner_demo.py
@@ -3,6 +3,8 @@ class Car:
     def __init__(self, make, yr):
         self.make=make
         self.year=yr
+    def display(self):
+        print(self.make,self.year)
 #inner cls
     class Engine:
         def __init__(self,no):
@@ -11,6 +13,17 @@ class Car:
         def start(self):
             print("Engine started")
 
+        def display(self):
+            print(self.number)
+
+
+
 c = Car("BMW", 2017)
 e=c.Engine(123)
 e.start()
+c.display()
+e.display()
+
+c1=Car("Audi",2019).Engine(999)
+c1.start()
+#c1.display()
