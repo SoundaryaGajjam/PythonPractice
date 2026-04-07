3272ccdf4806b474ae30dd3ff9664e2fd3728db9
Soundarya <soundaryagajjam@gmail.com>
2026-04-07 20:25:42 +0530

    star pattern

--- 2026_practice/Pattern/star.py
+++ 2026_practice/Pattern/star.py
@@ -1,5 +1,30 @@
-s = int(input("Enter a no"))
-
+#s = int(input("Enter a no"))
+s=5
 for i in range(1,s+1):
     print("* "*i)
     i+=1
+
+'''
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+'''
+
+print("---------------------------------------------------")
+n=5
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end="")
+    for k in range(i+1):
+        print("*",end=" ")
+    print()
+
+'''
+     * 
+    * * 
+   * * * 
+  * * * * 
+ * * * * * 
+ '''
